a0fa5ab0b1629d064df93a44427c7c05595efff7
vikaskumarbiddareddy <vikaskumar.biddreddy@gmail.com>
2024-08-02 08:52:24 +0530

    updated file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name = 'DimondPricePrediction',
+    version='0.0.1',
+    author='vikas kumar',
+    author_email='vikaskumar.biddareddy@gmail.com',
+    # """ install_requires = get_requirements('requirements.txt'), """
+    packages=find_packages() 
+)
