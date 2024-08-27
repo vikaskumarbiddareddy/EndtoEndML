b5f0151233fbec644f3114e80147f8e83bfe766c
vikaskumarbiddareddy <vikaskumar.biddareddy@gmail.com>
2024-08-27 07:32:06 +0530

    Model runs

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -99,7 +99,7 @@ class DataTransformation:
             
             input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
             target_feature_test_df=test_df[target_column_name]
-            
+             
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,5 +1,9 @@
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 
+from src.DimondPricePrediction.components.data_transformation import DataTransformation
+
+from src.DimondPricePrediction.components.model_trainer import ModelTrainer
+
 
 
 
@@ -12,6 +16,15 @@ import pandas as pd
 
 obj=DataIngestion()
 
-obj.initiate_data_ingestion()
+train_data_path, test_data_path = obj.initiate_data_ingestion()
+
+data_transformation_obj = DataTransformation()
+
+train_arr, test_arr = data_transformation_obj.initialize_data_transformation(train_data_path,test_data_path)
+
+modal_trainer_obj = ModelTrainer()
+
+modal_trainer_obj.initate_model_training(train_arr,test_arr)
+
 
 
